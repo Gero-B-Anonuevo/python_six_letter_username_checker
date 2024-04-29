b4718cc6ee4e79ae9f222cdd446e072dc12020c1
Gero-B-Anonuevo <150780703+Gero-B-Anonuevo@users.noreply.github.com>
2024-04-29 11:53:01 +0800

    Update prog.py

---
+++ prog.py
@@ -0,0 +1,5 @@
+name = input("Please input a name with only six characters: ")
+if len(name) == 6:
+    print("You have inserted a name with the length of six characters")
+elif len(name) != 6:
+    print("You have not inserted a name with with 6 characters length")
